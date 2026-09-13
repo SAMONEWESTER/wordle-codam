9b557c62ae13dd8c17b5d57a5d1f16d6d420c94a
Kim <codam>
2026-09-13 11:21:43 +0200

    used ruff format to make the code comply with PEP 8

--- wordle.py
+++ wordle.py
@@ -19,81 +19,89 @@ ANSI_DELETE = "\033[2K\r"
 ANSI_CLEAR = "\033[1A\033[2K\r"
 
 COLOR_DICTIONARY = {
-	WHITE: ANSI_WHITE,
-	GREEN: ANSI_GREEN,
-	YELLOW: ANSI_YELLOW,
-	GREY: ANSI_GREY
+    WHITE: ANSI_WHITE,
+    GREEN: ANSI_GREEN,
+    YELLOW: ANSI_YELLOW,
+    GREY: ANSI_GREY,
 }
 
+
 def print_in_colors(character, state):
-	print(COLOR_DICTIONARY[state] + character + ANSI_RESET, end="")
+    print(COLOR_DICTIONARY[state] + character + ANSI_RESET, end="")
+
 
 def print_current_state(guess, guess_state):
-	print(ANSI_CLEAR, end="")
-	for i in range(WORD_SIZE):
-		print_in_colors(guess[i], guess_state[i])
-	print()
+    print(ANSI_CLEAR, end="")
+    for i in range(WORD_SIZE):
+        print_in_colors(guess[i], guess_state[i])
+    print()
+
 
 def parse_dictionary(path):
-	with open(path, 'r') as file:
-		content = file.read()
-	wordlist = content.split()
-	return wordlist
+    with open(path, "r") as file:
+        content = file.read()
+    wordlist = content.split()
+    return wordlist
+
 
 def game_is_complete(guess_state):
-	for i in range(WORD_SIZE):
-		if guess_state[i] != GREEN:
-			return 0
-	return 1
+    for i in range(WORD_SIZE):
+        if guess_state[i] != GREEN:
+            return 0
+    return 1
+
 
 def update_current_state(word, guess):
-	guess_state = [WHITE] * WORD_SIZE
-	word_state = [WHITE] * WORD_SIZE
-	tuple_list = list(zip(word, guess))
-	i = 0
-	for tuple in tuple_list:
-		if (tuple[0] == tuple[1] or tuple[0] == tuple[1].lower()):
-			guess_state[i] = GREEN
-			word_state[i] = GREEN
-		i = i + 1
-	for i in range(WORD_SIZE):
-		if guess_state[i] != GREEN:
-			for j in range(WORD_SIZE):
-				if (word_state[j] == WHITE
-					and (word[j] == guess[i] or word[j] == guess[i].lower())
-				):
-					guess_state[i] = YELLOW
-					word_state[j] = YELLOW
-					break
-	for i in range(WORD_SIZE):
-		if guess_state[i] == WHITE:
-			guess_state[i] = GREY
-	return guess_state
+    guess_state = [WHITE] * WORD_SIZE
+    word_state = [WHITE] * WORD_SIZE
+    tuple_list = list(zip(word, guess))
+    i = 0
+    for tuple in tuple_list:
+        if tuple[0] == tuple[1] or tuple[0] == tuple[1].lower():
+            guess_state[i] = GREEN
+            word_state[i] = GREEN
+        i = i + 1
+    for i in range(WORD_SIZE):
+        if guess_state[i] != GREEN:
+            for j in range(WORD_SIZE):
+                if word_state[j] == WHITE and (
+                    word[j] == guess[i] or word[j] == guess[i].lower()
+                ):
+                    guess_state[i] = YELLOW
+                    word_state[j] = YELLOW
+                    break
+    for i in range(WORD_SIZE):
+        if guess_state[i] == WHITE:
+            guess_state[i] = GREY
+    return guess_state
+
 
 def guess_word(wordlist, word):
-	session = PromptSession(erase_when_done=True)
-	with session.app.input.raw_mode():
-		guess = session.prompt("")
-		while guess.lower() not in wordlist:
-			print(ANSI_DELETE + ANSI_RED + guess + ANSI_RESET, end="")
-			time.sleep(0.3)
-			print(ANSI_DELETE, end="")
-			guess = session.prompt("", default=guess)
-	print()
-	guess_state = update_current_state(word, guess)
-	return guess, guess_state
+    session = PromptSession(erase_when_done=True)
+    with session.app.input.raw_mode():
+        guess = session.prompt("")
+        while guess.lower() not in wordlist:
+            print(ANSI_DELETE + ANSI_RED + guess + ANSI_RESET, end="")
+            time.sleep(0.3)
+            print(ANSI_DELETE, end="")
+            guess = session.prompt("", default=guess)
+    print()
+    guess_state = update_current_state(word, guess)
+    return guess, guess_state
+
 
 def main():
-	wordlist = parse_dictionary("words.txt")
-	word = random.choice(wordlist)
-	# print(word)
-	counter = NUMBER_OF_GUESSES
-	while counter > 0:
-		guess, guess_state = guess_word(wordlist, word)
-		print_current_state(guess, guess_state)
-		if game_is_complete(guess_state):
-			break
-		counter = counter - 1
+    wordlist = parse_dictionary("words.txt")
+    word = random.choice(wordlist)
+    # print(word)
+    counter = NUMBER_OF_GUESSES
+    while counter > 0:
+        guess, guess_state = guess_word(wordlist, word)
+        print_current_state(guess, guess_state)
+        if game_is_complete(guess_state):
+            break
+        counter = counter - 1
+
 
 if __name__ == "__main__":
-	main()
+    main()
